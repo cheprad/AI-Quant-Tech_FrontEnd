c95940bb91e6769ead6a99063cd4b503adfece0a
cheprad <68316487+cheprad@users.noreply.github.com>
2022-03-02 19:11:48 +0700

    Create form2.php

---
+++ form2.php
@@ -0,0 +1,311 @@
+<!-- แบบฟอร์มบันทึกความรู้การลงทุน -->
+
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Robots Are The Future, Human-robot, The robots of CES 2019, Technology Trends, Artificial Intelligence Robots And The Future Of Work, Robots and AI, INTUITIVE">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+   <title>เรียนรู้การลงทุนกับ AI QUANT TECH ฟรี </title>
+
+    <link href="themes/assets/css/style.css" rel="stylesheet">
+
+    <!-- <meta name="generator" content="Nicepage 3.29.1, nicepage.com"> -->
+
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+    <link rel="stylesheet" href="Home.css" media="screen">
+
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+
+    <!-- from focus-premium -->
+    <link href="themes/assets/css/lib/font-awesome.min.css" rel="stylesheet">
+    <link href="themes/assets/css/lib/themify-icons.css" rel="stylesheet">
+    <link href="themes/assets/css/lib/owl.carousel.min.css" rel="stylesheet" />
+
+    
+    <!-- <link href="themes/assets/css/lib/owl.theme.default.min.css" rel="stylesheet" /> -->
+    <!-- <link href="themes/assets/css/lib/menubar/sidebar.css" rel="stylesheet"> -->
+    <!-- <link href="themes/assets/css/lib/bootstrap.min.css" rel="stylesheet"> -->
+    <!-- <link href="themes/assets/css/lib/helper.css" rel="stylesheet"> -->
+
+    <!-- cardcarousal  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.css"/>
+   <!-- form -->
+    <!-- <link href="form-validation.css" rel="stylesheet"> -->
+    
+    
+    
+    
+
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Home">
+    <meta property="og:type" content="website">
+  </head>
+  <body>
+      <header class="u-clearfix u-header u-header" id="sec-1ee7"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <a href="./" class="u-image u-logo u-image-1">
+          <img src="images/thislogo.png" class="u-logo-image u-logo-image-1">
+        </a>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;"><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</symbol>
+</defs></svg>
+            </a>
+          </div>
+          <div class="u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1">
+              <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="index.html" style="padding: 10px 20px;">หน้าแรก</a></li>
+              <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="./index.html#About_us" style="padding: 10px 20px;">เกี่ยวกับเรา</a></li>
+              <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="./index.html#Service" style="padding: 10px 20px;">บริการของเรา</a></li>
+              <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="./index.html#contact_us" style="padding: 10px 20px;">ติดต่อเรา</a></li>
+              <li class="u-nav-item"><a class= "btn btn-primary" href="http://aiquanttech.com/" target="_blank" style="padding: 10px 20px;">เริ่มต้นลงทุน</a></li>
+            </ul>
+            </div>
+          
+        </nav>
+      </div>
+    </header>
+    <div class="container" style=" max-width: 700px;">
+    <main>
+        <div class="py-5 text-center">
+          <h2>เรียนรู้การลงทุนกับ AI QUANT TECH ฟรี</h2>
+        </div>
+        <div class="row g-3 " >
+          
+         
+            <h4 class="mb-3" >ข้อมูลทั่วไป</h4>
+            <form action="./upload.php" class="needs-validation" method="post" enctype="multipart/form-data" novalidate >
+                
+              <div class="row g-3">
+ 
+                <div class="col-sm-6">
+                  <label for="firstName" class="form-label">ชื่อจริง</label>
+                  <input type="text" class="form-control" id="firstName" name="first_name"  required>
+                  <div class="invalid-feedback">
+                    Valid first name is required.
+                  </div>
+                </div>
+    
+                <div class="col-sm-6">
+                  <label for="lastName" class="form-label">นามสกุล</label>
+                  <input type="text" class="form-control" id="lastName" name="last_name" required>
+                  <div class="invalid-feedback">
+                    Valid last name is required.
+                  </div>
+                </div>
+                <div class="col-6">
+                  <label for="email" class="form-label">Email</label>
+                  <input type="email" class="form-control" id="email" name="email" placeholder="you@example.com" required>
+                  <div class="invalid-feedback">
+                    กรุณาใส่ Email 
+                  </div>
+                </div> 
+                <div class="col-6">
+                  <label for="email" class="form-label">เบอร์โทรศัพย์</label>
+                  <input type="tel" class="form-control" id="email" name="email" placeholder="(000)-000-0000" required>
+                  <!-- <input type="tel" id="input_6_full" name="q6_input6[full]" data-type="mask-number" class="" data-defaultvalue="" autocomplete="section-input_6 tel-national" style="width:310px" data-masked="true" value="" placeholder="(000) 000-0000" data-component="phone" aria-labelledby="label_6" required="" inputmode="text" maskvalue="(###) ###-####"> -->
+                  <div class="invalid-feedback">
+                    กรุณาใส่ Email 
+                  </div>
+                </div>  
+
+                <div class="col-6">
+                  <label for="gender" class="form-label">เพศ</label>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                    <label class="form-check-label" for="flexRadioDefault1">
+                      ชาย
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                    <label class="form-check-label" for="flexRadioDefault2">
+                      หญิง
+                    </label>
+                  </div>
+
+                </div>
+
+                
+                <div class="col-6">
+                  <label for="age" class="form-label">อายุ</label>
+                  <select class="form-select" id="age" name="age">
+                    <option value="18-">ต่ำกว่า 18 ปี</option>
+                    <option value="19-30">19 - 30 ปี</option>
+                    <option value="31-40">31 - 40 ปี</option>
+                    <option value="41-60">51 - 60 ปี</option>
+                    <option value="61+">61 ปีขึ้นไป</option>
+                  </select>
+                  <div class="invalid-feedback">
+                    กรุณาเลือกช่วงอายุของท่าน
+                  </div>
+
+                </div>
+                <hr class="my-4">
+                <div class="col-6">
+                  <label for="่job" class="form-label">อาชีพ</label>
+                  <select class="form-select" id="่job" name="job">
+                    <option value="18-">นักเรียนนักศึกษา</option>
+                    <option value="19-30">พนักงานเอกชน</option>
+                    <option value="31-40">พนักงานราชการ</option>
+                    <option value="41-60">เจ้าของกิจการ</option>
+                    <option value="61+">Full-time Trader (ว่างงาน/ฟรีแลนซ์)</option>
+                  </select>
+                  <div class="invalid-feedback">
+                    กรุณาเลือกอาชีพของท่าน
+                  </div>
+                </div>
+
+                <div class="col-6">
+                  <label for="่income" class="form-label">รายได้ต่อเดือน</label>
+                  <select class="form-select" id="่income" name="income">
+                    <option value="-10000">ต่ำกว่า 10,000 บาท</option>
+                    <option value="10001-20000">10,001 - 20,000 บาท</option>
+                    <option value="20001-30000">20,001 - 30,000 บาท</option>
+                    <option value="30001-40000">30,001 - 40,000 บาท</option>
+                    <option value="40001+">40,001 บาทขึ้นไป</option>
+                  </select>
+                  <div class="invalid-feedback">
+                    กรุณาเลือกช่วงรายได้ของท่าน
+                  </div>
+                </div>
+
+                <div class="col-6">
+                  <label for="portfolio-status" class="form-label">มีพอร์ตการลงทุนรึยัง</label>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                    <label class="form-check-label" for="flexRadioDefault1">
+                      มี
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                    <label class="form-check-label" for="flexRadioDefault2">
+                      ไม่มี
+                    </label>
+                  </div>
+                </div>
+
+                <div class="col-6">
+                  <label for="experience" class="form-label">ประสบการณ์ในการเทรด</label>
+                  <select class="form-select" id="่income" name="income">
+                    <option value="0-1">0 - 1 ปี</option>
+                    <option value="1-2">1 - 2 ปี</option>
+                    <option value="3+">3 ปีขึ้นไป</option>
+                  </select>
+                  <div class="invalid-feedback">
+                    กรุณาเลือกประสบการณ์ในการเทรด
+                  </div>
+                </div>
+
+                <div class="col-6">
+                  <label for="invested-product" class="form-label">ผลิตภัณฑ์ที่ท่านเคยใช้ในการลงทุน</label>
+                  <select class="form-select" id="invested-product" name="invested-product">
+                    <option value="">ไม่เคย</option>
+                    <option value="10001-20000">หุ้น</option>
+                    <option value="20001-30000">TFEX</option>
+                    <option value="20001-30000">DW</option>
+                    <option value="20001-30000">WARRANT</option>
+                    <option value="20001-30000">Cryptocurrency</option>
+                    <option value="20001-30000">ทองคำ</option>
+                  </select>
+                  <div class="invalid-feedback">
+                    กรุณาเลือกประสบการณ์ในการเทรด
+                  </div>
+                </div>
+                <hr class="my-4">
+                <div class="col-12">
+                  <label for="invested-product" class="form-label">ทำไมท่านถึงสนใจเข้าร่วมเรียนรู้การลงทุนฟรี กับ AI Quant Tech  (มากสุด 1 ข้อ )</label>
+                  <select class="form-select" id="invested-product" name="invested-product">
+                    <option value="">ไม่เคย</option>
+                    <option value="อยากเรียนรู้การลงทุน">อยากเรียนรู้การลงทุน</option>
+                    <option value="อยากมีรายได้เสริม">อยากมีรายได้เสริม</option>
+                    <option value="อยากให้เป็นอาชีพหลักในอนาคต">อยากให้เป็นอาชีพหลักในอนาคต</option>
+                    <option value="อยากมีเพื่อน พูดคุยการลงทุน">อยากมีเพื่อน พูดคุยการลงทุน</option>
+                    <option value="อยากเตรียมแผนการเกษียณ">อยากเตรียมแผนการเกษียณ</option>
+                    <option value="อื่นๆ">อื่นๆ</option>
+                  </select>
+                  <div class="invalid-feedback">
+                    กรุณาเลือกประสบการณ์ในการเทรด
+                  </div>
+                </div>
+
+    
+                <!-- <div class="col-12">
+                  <label for="username" class="form-label">Username</label>
+                  <div class="input-group has-validation">
+                    <span class="input-group-text">@</span>
+                    <input type="text" class="form-control" id="username" placeholder="Username" required>
+                  <div class="invalid-feedback">
+                      Your username is required.
+                    </div>
+                  </div>
+                </div> -->
+    
+            
+    
+
+                
+    
+    
+          
+              <hr class="my-4">
+              <button class="w-100 btn btn-primary btn-lg" name="submit" type="submit" value="upload" >ยืนยัน</button>
+            </form>
+        </div>
+      </main>
+    </div>
+
+    <section>
+      <a name="contact_us"></a>
+    </section>
+    <footer 
+    class="u-align-center u-clearfix u-footer u-grey-80 u-footer" 
+    id="sec-1400"
+    style="padding-bottom: 40px;">
+      <container style="top: 50px;">
+        <h3>โทรศัพท์ 095-824-4526</h3>
+        <a href="https://lin.ee/TDgEuGS">
+          <img src="images/main/LINE_MSG.svg" width="50px" alt="">
+        </a>
+        &nbsp;&nbsp;
+        <a href="https://m.me/AIquantTech">
+          <img src="images/main/FB_MSG.svg"  width="50px"alt="">
+        </a>
+        <p class="u-small-text u-text u-text-variant u-text-1" style="margin-top: 20px;">
+            บริษัท เอไอ ควอนเทค จำกัด 
+            72/230 โครงการ Centro พหล-วิภาวดีรังสิต พหลโยธิน ต. คลองหนึ่ง อ. คลองหลวง จ.ปทุมธานี 12120
+        </p>
+      </container>
+     </footer>
+     <!-- jquery vendor -->
+     <script src="themes/assets/js/lib/jquery.min.js"></script>
+     <script src="themes/assets/js/lib/jquery.nanoscroller.min.js"></script>
+     <!-- nano scroller -->
+     <script src="themes/assets/js/lib/menubar/sidebar.js"></script>
+     <script src="themes/assets/js/lib/preloader/pace.min.js"></script>
+     <!-- sidebar -->
+     
+     <!-- bootstrap -->
+     <!-- <script src="themes/assets/js/lib/owl-carousel/owl.carousel.min.js"></script>
+     <script src="themes/assets/js/lib/owl-carousel/owl.carousel-init.js"></script> -->
+     <script src="themes/assets/js/lib/bootstrap.min.js"></script>
+     <script src="themes/assets/js/scripts.js"></script>
+     <!-- scripit init-->
+     <script src="https://getbootstrap.com/docs/5.0/examples/checkout/form-validation.js"></script>
+
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.js"></script>
+     <script src="scripts.js"></script>
+  </body>
+</html>
